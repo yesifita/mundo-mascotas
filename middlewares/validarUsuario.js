7717d2e9decbade6a120b0577e8bc2c49ebc1b59
yesi fita <yesifita@gmail.com>
2022-12-08 20:03:08 -0300

    corrigo middleware de validacion de usuario

--- middlewares/validarUsuario.js
+++ middlewares/validarUsuario.js
@@ -1,5 +1,5 @@
 const validarUsuario= (req,res,next)=>{
-    if (!req.session) {
+    if (!req.session.usuario) {
         res.status(400).json({msg:"sesion no iniciada"})
     } else {
         next();
